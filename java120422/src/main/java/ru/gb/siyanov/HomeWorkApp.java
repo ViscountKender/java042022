4601300c18bd1d229a5a1f79b3f555436d5ab024
Viscount <siyanov.d@sar.garm.ru>
2022-04-12 23:47:50 +0400

    Did all the tasks

--- java120422/src/main/java/ru/gb/siyanov/HomeWorkApp.java
+++ java120422/src/main/java/ru/gb/siyanov/HomeWorkApp.java
@@ -7,10 +7,51 @@ public class HomeWorkApp {
         System.out.println("Banana");
         System.out.println("Apple");
     }
+    public static void checkSumSign(){
+        int a = 10;
+        int b = 20;
+        if (a + b >= 0 ){
+            System.out.println("Сумма положительная");
 
+        }else {
+            System.out.println("Сумма отрицательная");
 
-       public static void main (String[]args){
-
-
-            }
         }
+
+    }
+    public static void printColor (){
+        int value = -100;
+        if (0 >= value){
+            System.out.println("Красный");
+
+        }
+        else if (0 < value && value <=100 ){
+            System.out.println("Желтый");
+
+        }
+        else {
+            System.out.println("Желтый");
+
+        }
+
+    }
+    public static void compareNumbers(){
+        int a = 200;
+        int b = -5;
+        if (a >= b){
+            System.out.println("a >= b");
+
+        }
+        else {
+            System.out.println("a < b");
+        }
+
+    }
+    public static void main(String[] args) {
+        printThreeWords ();
+        checkSumSign();
+        printColor ();
+        compareNumbers();
+
+    }
+}
