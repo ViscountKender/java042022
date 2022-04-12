f8bbdde9bfb7dc3296795efce6cbcfefa9774cf6
Viscount <siyanov.d@sar.garm.ru>
2022-04-12 23:08:13 +0400

    first task

--- java120422/src/main/java/ru/gb/siyanov/HomeWorkApp.java
+++ java120422/src/main/java/ru/gb/siyanov/HomeWorkApp.java
@@ -1,7 +1,16 @@
 package ru.gb.siyanov;
 
 public class HomeWorkApp {
-    public static void main(String[] args) {
 
+    public static void printThreeWords (){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
     }
-}
+
+
+       public static void main (String[]args){
+
+
+            }
+        }
